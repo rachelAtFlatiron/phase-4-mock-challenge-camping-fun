8afc6875aca1f0032ba125d635856fee2a976151
Rachel Katz <rachel.katz@flatironschool.com>
2022-06-14 10:35:02 -0400

    commit tree 019a2eba3d9148fcb4ef028b2f96f0a41b6aba80

---
+++ app/controllers/campers_controller.rb
@@ -0,0 +1,33 @@
+class CampersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    def index
+        render json: Camper.all, status: 200 
+    end 
+
+    def show 
+        camper = Camper.find(params[:id])
+        render json: camper, serializer: CamperWithActivitiesSerializer
+    end 
+
+    def create
+        camper = Camper.create!(camper_params)
+        render json: camper, status: :created  
+    end 
+
+    private 
+
+    def camper_params
+        params.permit(:name, :age) 
+    end 
+
+    def render_not_found_response
+        render json: {error: "Camper not found"}, status: :not_found
+    end 
+
+    def render_unprocessable_entity_response 
+        render json: {error: exception.record.errors.full_messages }, status: :unprocessable_entity
+    end 
+
+
+end
